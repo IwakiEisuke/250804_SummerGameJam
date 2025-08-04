0c8137e9babc3a4e9b125c64d4b9e994e5ada61f
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2025-08-04 15:59:06 +0900

    [add]BGMのスタート処理を追加、ignoreの更新

---
+++ Assets/Shiomi/Scripts/BGMStarter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGMStarter : MonoBehaviour
+{
+    //シーンスタート時にBGMを流す
+    void Start()
+    {
+        //現在のシーンを取得
+        var currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        //シーンに合わせてBGMを流す
+        switch(currentScene)
+        {
+            case 0:
+                SoundManager.Instance.PlayBGM(BGMSoundData.BGM.Title);
+                break;
+            case 1:
+                SoundManager.Instance.PlayBGM(BGMSoundData.BGM.Ingame);
+                break;
+            case 2:
+                SoundManager.Instance.PlayBGM(BGMSoundData.BGM.Result);
+                break;
+        }
+    }
+}
