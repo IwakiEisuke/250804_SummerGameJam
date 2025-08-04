c308da6e16fc7f54e5165f83ae83b43337c242cc
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-08-04 13:22:37 +0900

    [add] ランキングのエントリ表示コンポーネント

---
+++ Assets/Iwaki/RankingItemView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingItemView : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+
+    public void SetData(RankingItem item, int rankIndex)
+    {
+        if (item == null)
+        {
+            scoreText.text = "N/A"; // データがない場合の表示
+        }
+        else
+        {
+            scoreText.text = $"{rankIndex + 1}位 {item.score}杯"; // ランキングとスコアを表示
+        }
+    }
+}
