abddf1f08b9624b81fdb45926e2e0c16c056a9f4
IwakiEisuke <get10.ref11.del12@gmail.com>
2025-08-04 13:20:10 +0900

    [add] ランキングの読み取り・書き込み処理

---
+++ Assets/Iwaki/RankingView.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class RankingView : MonoBehaviour
+{
+    [SerializeField] Transform _rankingBoardRoot;
+    [SerializeField] DataManager _dataManager;
+
+    RankingData _rankingData; // ランキングデータのインスタンス
+
+    private void Start()
+    {
+        _rankingData = _dataManager.Data;
+    }
+}
+
+/// <summary>
+/// ランキング情報の１個ずつ
+/// </summary>
+[System.Serializable]
+public class RankingItem
+{
+    public int score;
+}
+
+/// <summary>
+/// Jsonで保存するランキングデータ
+/// </summary>
+[System.Serializable]
+public class RankingData
+{
+    public const int rankCount = 3;
+    public RankingItem[] ranks = new RankingItem[rankCount];
+}
