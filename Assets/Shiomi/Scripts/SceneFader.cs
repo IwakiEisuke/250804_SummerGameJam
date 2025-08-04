da6cbc69ad33ff56615857c01a41f9e102f0207a
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2025-08-04 14:41:00 +0900

    [add]シーンローダーの追加

---
+++ Assets/Shiomi/Scripts/SceneFader.cs
@@ -0,0 +1,38 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SceneFader : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _fadeCanvasGroup;
+
+    public UnityEvent onFadeInComplete;
+    public UnityEvent onFadeOutComplete;
+
+    private void Start()
+    {
+        _fadeCanvasGroup.alpha = 1.0f;
+        FadeIn();
+    }
+
+    /// <summary>
+    /// フェードインの関数
+    /// </summary>
+    /// <param name="duration">完了時間</param>
+
+    public void FadeIn(float duration = 3)
+    {
+        _fadeCanvasGroup.DOFade(0f, duration).SetEase(Ease.InOutQuad)
+            .OnComplete(() => onFadeInComplete.Invoke());
+    }
+
+    /// <summary>
+    /// フェードアウトの関数
+    /// </summary>
+    /// <param name="duration">完了時間</param>
+    public void FadeOut(float duration)
+    {
+        _fadeCanvasGroup.DOFade(1f, duration).SetEase(Ease.InOutQuad)
+            .OnComplete(() => onFadeOutComplete.Invoke());
+    }
+}
