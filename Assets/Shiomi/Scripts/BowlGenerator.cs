74806254399da879e999d03112c9a68b991e19cc
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2025-08-05 16:00:25 +0900

    [fix]インゲームの修正

--- Assets/Shiomi/Scripts/BowlGenerator.cs
+++ Assets/Shiomi/Scripts/BowlGenerator.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
@@ -12,12 +13,57 @@ public class BowlGenerator : MonoBehaviour
 
     [SerializeField] private BowlCounter _bowlCounter;
     
-    private int indexCounter = 0;
+    private int _indexCounter = 0;
     private int _currentBowlCount = 0;
+    private int _rowIndex = 0;
+    private int _lineIndex = 0;
+    private bool _isRight = true;
 
+    private void Start()
+    {
+        _currentBowlCount += _firstBowlIntervalCount[_indexCounter];
+    }
 
     public void GenerateBowl()
     {
+        if (_indexCounter >= _bowlObjects.Length)
+            return;
+
+        //ボールをカウント
         int currentBowlCount = _bowlCounter.bowlCount;
+
+        if(currentBowlCount == _currentBowlCount)
+        {
+
+            if(_isRight)
+            {
+                Instantiate(_bowlObjects[_indexCounter], 
+                    new Vector3(_firstRightPos.position.x + _widthInterval * _rowIndex, _firstRightPos.position.y, 
+                    _firstRightPos.position.z + _verticalInterval *_lineIndex), Quaternion.identity);
+
+                _isRight = false;
+                Debug.Log("お椀生成");
+            }
+            else
+            {
+                Instantiate(_bowlObjects[_indexCounter],
+                    new Vector3(_firstLeftPos.position.x - _widthInterval * _rowIndex, _firstLeftPos.position.y,
+                    _firstLeftPos.position.z + _verticalInterval * _lineIndex), Quaternion.identity);
+
+                _rowIndex ++;
+                _isRight = true;
+
+                if (_rowIndex >= 3)
+                {
+                    _lineIndex++;
+                    _indexCounter++;
+                    _rowIndex = 0;
+                }
+
+                Debug.Log("お椀生成");
+            }
+
+            _currentBowlCount += _firstBowlIntervalCount[_indexCounter];
+        }
     }
 }
